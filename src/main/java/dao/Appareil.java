9b2719ae35bba750005af303a258898b9a235ac9
rimoubalkas-svg <rim.oubalkas@gmail.com>
2025-12-08 23:14:00 +0100

    test

--- src/main/java/dao/Appareil.java
+++ src/main/java/dao/Appareil.java
@@ -1,5 +1,25 @@
 package dao;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Appareil {
 
+    @Id
+    @Column(length = 20)   // IMEI is usually 15–17 digits
+    private String imei;
+
+    @Column(length = 50)
+    private String type;
 }
--- src/main/java/dao/Emprunt.java
+++ src/main/java/dao/Emprunt.java
@@ -1,5 +1,29 @@
 package dao;
 
+import javax.persistence.*;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Emprunt {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double montant;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateEmprunt;
+
+    @Column(length = 100)
+    private String motif;
 }
