1a6bde888ae7ccd89883373eb8383f5df41332df
shawskyamine <amineouhmidouu@gmail.com>
2025-12-08 22:49:49 +0100

    modifs proprietaire

---
+++ src/main/java/dao/Proprietaire.java
@@ -0,0 +1,28 @@
+package dao;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Proprietaire extends Utilisateur {
+
+    @Column
+    private double caisseGlobale;
+
+    @Column
+    private double comptePersonnel;
+
+}
